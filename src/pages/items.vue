bbefb672b3e1640a3af3e0eda118eb576396e4da
정수진 <sssjsjj@ipartners.co.kr>
2022-07-12 11:16:02 +0900

    feat: 아이템도감 페이지 생성 / 선원 도감 - 아이템 필터링 로직

---
+++ src/pages/items.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="top-bar">
+      <ul>
+        <li
+          v-for="(subMenu, i) in subMenus"
+          :key="`subMenu${i}`"
+        >
+          <nuxt-link :to="'/items/' + subMenu.name">{{ subMenu.title }}</nuxt-link>
+        </li>
+      </ul>
+    </div>
+    <div class="inner-size-basic">
+      <nuxt-child></nuxt-child>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      subMenus: [
+        { title: '선원', name: 'sailor' },
+        { title: '동료', name: 'colleague' },
+        { title: '선박', name: 'ship'}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
