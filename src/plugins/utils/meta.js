14cb49b81347d0847656ff640395285245927b89
정수진 <sssjsjj@ipartners.co.kr>
2022-12-15 22:38:21 +0900

    refactor: 문자열 처리 연산자 개선

--- src/plugins/utils/meta.js
+++ src/plugins/utils/meta.js
@@ -5,21 +5,21 @@ const setMeta = (metaObj = {}) => {
   if(Object.keys(metaObj).length === 0) return []
 
   // description
-  const descContent = description ? defaultDesc + description : defaultDesc
+  const descContent = defaultDesc + (description || '')
   resultMeta.push({ hid: 'description', name: 'description', content: descContent })
   resultMeta.push({ hid: 'og-description-value', property: 'og:description', content: descContent })
   resultMeta.push({ hid: 'twitter-description-value', name: 'twitter:description', content: descContent })
   resultMeta.push({ hid: 'schema-discription-value', itemprop: 'description', content: descContent })
 
   // url
-  const urlContent = url ? process.env.BASE_URL + url : process.env.BASE_URL
+  const urlContent = process.env.BASE_URL + (url || '')
   resultMeta.push({ hid: 'og-url-value', property: 'og:url', content: urlContent })
   resultMeta.push({ hid: 'twitter-url-value', name: 'twitter:url', content: urlContent })
   resultMeta.push({ hid: 'schema-url-value', itemprop: 'url', content: urlContent })
     
   // title
   const defaultTitle = `${process.env.APP_TITLE} | Warcraft3 Onepiece RPG 정보 제공 사이트`
-  const titleContent = title ? title + ` | ${defaultTitle}` : defaultTitle
+  const titleContent = (title ? `${title} | ` : '') + defaultTitle
   resultMeta.push({ title: titleContent })
   resultMeta.push({ hid: 'og-title-value', property: 'og:title', content: titleContent })
   resultMeta.push({ hid: 'twitter-title-value', name: 'twitter:title', content: titleContent })
