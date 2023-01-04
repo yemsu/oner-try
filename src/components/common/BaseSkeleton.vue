3ab97ebce9e8a8a7288a6c885abe5fc8b34a190b
정수진 <=>
2023-01-04 17:31:19 +0900

    feat: 검색 top 10 스켈레톤 ui 작업

---
+++ src/components/common/BaseSkeleton.vue
@@ -0,0 +1,53 @@
+<template>
+  <span
+    :class="`skeleton ${type} size-${size}`"
+    :style="type === 'text' && `width: ${width};`"
+  >
+    skeleton
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'text' // img, text
+    },
+    size: {
+      type: String,
+      default: 'medium' // medium, small
+    },
+    width: {
+      type: String,
+      default: '100px'
+    }
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.skeleton {
+  overflow: hidden;
+  display: inline-block;
+  text-indent: -999em;
+  background-color: var(--bg-light-gray);
+  &.text {
+    display: block;
+    height: 19px;
+    border-radius: 5px;
+  }
+  &.img {
+    margin: 6px;
+    border-radius: 100%;
+    &.size-small {
+      width: $item-image-small;
+      height: $item-image-small;
+    }
+  }
+}
+</style>
