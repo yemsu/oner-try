5fb5b3e955e69704e682442c016d6f89c8b9facf
정수진 <sssjsjj@ipartners.co.kr>
2023-01-17 23:01:07 +0900

    feat: 한정 재화 가치 산정 기준 정리

---
+++ src/plugins/utils/exchange.js
@@ -0,0 +1,127 @@
+export const bountyValues = [
+  {
+    min: 1,
+    max: 3000,
+    value: 20
+  },
+  {
+    min: 3001,
+    max: 8000,
+    value: 30
+  },
+  {
+    min: 8001,
+    max: 15000,
+    value: 50
+  },
+  {
+    min: 15001,
+    max: 30000,
+    value: 50000
+  },
+]
+
+export const levelValues = [
+  {
+    min: 1,
+    max: 300,
+    value: 40
+  },
+  {
+    min: 301,
+    max: 600,
+    value: 100
+  },
+  {
+    min: 601,
+    max: 800,
+    value: 300
+  },
+  {
+    min: 801,
+    max: 840,
+    value: 10000
+  },
+  {
+    min: 840,
+    max: 850,
+    value: 50000
+  },
+]
+
+export const accessoryValues = [
+  {
+    stack: 1,
+    value: 1000
+  },
+  {
+    stack: 2,
+    value: 3000
+  },
+  {
+    stack: 3,
+    value: 5000
+  },
+  {
+    stack: 4,
+    value: 10000
+  },
+  {
+    stack: 5,
+    value: 50000
+  },
+]
+
+export const crownNoseValues = [
+  {
+    type: '완벽함',
+    min: 1,
+    max: 89,
+    value: 150
+  },
+  {
+    type: '완벽함',
+    min: 90,
+    max: 100,
+    value: 1500
+  },
+]
+
+export const kuronekoValues = [
+  {
+    type: '유명함장비',
+    min: 1,
+    max: 89,
+    value: 100
+  },
+  {
+    type: '유명함장비',
+    min: 90,
+    max: 100,
+    value: 300
+  },
+]
+
+export const cliqueValues = [
+  {
+    type: '황금빛광대의코',
+    min: 1,
+    max: 89,
+    value: 300
+  },
+  {
+    type: '황금빛광대의코',
+    min: 90,
+    max: 100,
+    value: 2500
+  },
+]
+
+export const fujitoraValues = [
+  {
+    type: '유명함장비',
+    min: 1,
+    max: 89,
+    value: 
+  }
+]
