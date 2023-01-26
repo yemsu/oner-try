63055258e1f7da702b2c947b1d15b42da7c80c30
정수진 <sssjsjj@gmail.com>
2023-01-26 10:58:56 +0900

    feat: 깃험 로그인 추가(블로그 업로드용 코드)

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,96 +1,72 @@
 <template>
-  <section class="wrap-content">
-    <div>
-      <h2>로그인</h2>
-      <button v-if="!isLogin" @click="loginProcess">구글 로그인</button>
-      <template v-else>
-        <p>{{ userEmail }}</p>
-        <p>{{ userName }}</p>
-        <button @click="clickLogout()">로그아웃</button>
-        <button @click="getUserNick">get nick</button>
-      </template>
-    </div>
+  <section>
+    <h2>로그인</h2>
+    <input type="text" v-model="userEmailValue">
+    <input type="password" v-model="userPasswordValue">
+    <template v-if="!isLogin">
+      <button @click="onClickLogin('google')">구글 로그인</button>
+      <button @click="onClickLogin('github')">깃헙 로그인</button>
+    </template>
+    <template v-else>
+      <p>{{ userEmail }}</p>
+      <p>{{ userName }}</p>
+      <button @click="onClickLogout()">로그아웃</button>
+    </template>
   </section>
 
 </template>
 
 <script>
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 import { auth } from "@/plugins/firebase"
-import { postSocialLogin, getUserInfo } from "@/plugins/utils/https"
 
 export default {
   data() {
     return {
-      emailInputValue: '',
+      userEmailValue: '',
+      userPasswordValue: '',
       userEmail: null,
       userName: null,
       isLogin: false
     }
   },
-  created() {
-  },
-  mounted() {
-    console.log('mount', localStorage.getItem('accessToken'))
-    if(localStorage.getItem('accessToken')) {
-      this.isLogin = true
-      // 유저 이름, 이메일도 다시 받아와야함.
-    }
-    console.log('1.5', this.isLogin)
-  },
   methods: {
-    updateEmail(value) {
-      this.emailInputValue = value
+    async onClickLogin(serviceName) {
+      const LoginData = await this.snsPopupLogin(serviceName)      
+      if(!LoginData) return
+      this.isLogin = true
+      this.userEmail = LoginData.user.email
+      this.userName = LoginData.user.displayName
     },
-    async loginProcess() {
-      const googleLoginData = await this.googleLogin()
-      this.postLoginData(googleLoginData)
-    },
-    async postLoginData() {
-      try {
-        const token = await postSocialLogin({email: this.userEmail})
-        localStorage.setItem('accessToken', token)
-        // localStorage.setItem('refreshToken', )
-        // localStorage.setItem('expiredTime', )
-        this.isLogin = true
-      } catch(e) {
-        console.error('postLoginData error', e)
+    async snsPopupLogin(serviceName) {
+      let provider
+      if(serviceName === 'google') {
+        provider = new GoogleAuthProvider()
+      } else if(serviceName === 'github') {
+        provider = new GithubAuthProvider()
       }
-    },
-    async getUserNick() {
-      console.log('getUserNick')
-      const nick = await getUserInfo()
-      console.log('nick', nick)
-    },
-    async googleLogin() {
-      try {
-        const provider = new GoogleAuthProvider()
-        const data = await signInWithPopup(auth, provider)
-        console.log(data)
-        this.userEmail = data.user.email
-        this.userName = data.user.displayName
-        return true
-      } catch(e) {
-        alert('팝업 차단 해제 후 로그인 해주세요!')
-      }
-    },
-    clickLogout() {
-      console.log('clickLogout', auth, auth.signOut)
-      try {
-        const signout = auth.signOut()
-        console.log('signout', signout)
 
-        localStorage.removeItem('accessToken')
+      try {
+        const res = await signInWithPopup(auth, provider)
+        return res
+      } catch(e) {
+        if(e.code.includes('popup-blocked')) {
+          alert('팝업 차단 해제 후 로그인 해주세요!')
+        } else {
+          console.error('snsPopupLogin : ', e, e.code)
+        }
+      }    
+    },
+    onClickLogout() {
+      try {
+        auth.signOut()
+        this.isLogin = false
         this.userEmail = null
         this.userName = null
-        this.isLogin = false
       } catch(e) {
-        console.error('로그아웃 실패')
+        console.error('로그아웃 실패 : ', e)
       }
     },
   }
 }
 </script>
-
-<style>
-</style>
