32e312797c03935602615838fdfe6a563f399e48
정수진 <sssjsjj@gmail.com>
2023-02-09 13:47:40 +0900

    fix: 오타 수정 및 메타 값 통일성 개선

--- src/plugins/utils/meta.js
+++ src/plugins/utils/meta.js
@@ -1,7 +1,7 @@
 const setMeta = (metaObj = {}) => {
   const { description, url, title  } = metaObj
   const resultMeta = []
-  const defaultDesc = '워크3 원피스 RPG 정보 제공 사이트 ONER TRY. '
+  const defaultDesc = 'Warcraft3 Onepiece RPG 정보 제공 사이트 ONER TRY. '
   if(Object.keys(metaObj).length === 0) return []
 
   // description
@@ -9,7 +9,7 @@ const setMeta = (metaObj = {}) => {
   resultMeta.push({ hid: 'description', name: 'description', content: descContent })
   resultMeta.push({ hid: 'og-description-value', property: 'og:description', content: descContent })
   resultMeta.push({ hid: 'twitter-description-value', name: 'twitter:description', content: descContent })
-  resultMeta.push({ hid: 'schema-discription-value', itemprop: 'description', content: descContent })
+  resultMeta.push({ hid: 'schema-description-value', itemprop: 'description', content: descContent })
 
   // url
   const urlContent = process.env.BASE_URL + (url || '')
@@ -18,7 +18,7 @@ const setMeta = (metaObj = {}) => {
   resultMeta.push({ hid: 'schema-url-value', itemprop: 'url', content: urlContent })
     
   // title
-  const defaultTitle = `${process.env.APP_TITLE} | Warcraft3 Onepiece RPG 정보 제공 사이트`
+  const defaultTitle = `${process.env.NODE_ENV === 'development' ? 'LOCAL | ' : ''}${process.env.APP_TITLE} | 워크래프트3 원피스 RPG 정보 제공 사이트`
   const titleContent = (title ? `${title} | ` : '') + defaultTitle
   resultMeta.push({ title: titleContent })
   resultMeta.push({ hid: 'og-title-value', property: 'og:title', content: titleContent })
