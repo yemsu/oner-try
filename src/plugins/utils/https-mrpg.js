c2be7b1b204941b00f48bfc199d87e36cf761712
정수진 <sssjsjj@gmail.com>
2023-02-15 14:22:21 +0900

    fix: 이전 errorHandler 복붙

--- src/plugins/utils/https-mrpg.js
+++ src/plugins/utils/https-mrpg.js
@@ -12,3 +12,26 @@ function getApi(url = '', params = { params: {} }) {
     .then(({ data }) => data)
     .catch(error => errorHandler(url, error))
 }
+
+const errorHandler = (actionName, error) => {
+  const { response, request, message, config } = error
+  if (response) {
+    const { data, status, headers } = response
+    // if(status !== 200) {
+    //   window.$nuxt.$router.push(getErrorPageRouteData(status))
+    // }
+    // 요청이 전송되었고, 서버는 2xx 외의 상태 코드로 응답했습니다.
+    console.log(`${actionName} : data`, data);
+    console.log(`${actionName} : status`, status);
+    console.log(`${actionName} : headers`, headers);
+  } else if (request) {
+    // 요청이 전송되었지만, 응답이 수신되지 않았습니다. 
+    // 'error.request'는 브라우저에서 XMLHtpRequest 인스턴스이고,
+    // node.js에서는 http.ClientRequest 인스턴스입니다.
+    console.log(`${actionName} : request`, request);
+  } else {
+    // 오류가 발생한 요청을 설정하는 동안 문제가 발생했습니다.
+    console.log(`${actionName} : Error`, message);
+  }
+  console.log(`${actionName} : config`, config);
+}
