ebeb453e1ce709ec4c28012c665844cfbbe3b918
정수진 <sssjsjj@ipartners.co.kr>
2023-05-04 20:38:38 +0900

    feat: 메인 페이지 디자인 리뉴얼

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,105 +1,115 @@
 <template>
-  <div class="inner-size-basic">
-    <div class="wrap-quick-menu">
-      <!-- <section class="column-full point-banner">
-        <div class="box-flex">
-          <div class="item-flex area-img">
-            <img src="@/assets/images/character-update-220722.jpg" alt="">
-          </div>
-          <div class="item-flex about">
-            <div class="area-text">
-              <h3 class="title">🤹‍♂️ 캐릭터 페이지에 기능이 추가되었습니다 🎉</h3>
-              <p>류오 / 시너지 / 스탯이 추가되었어요!</p>
+  <div class="main" :style="`background-image: url(${BASE_URL}/images/main-key-visual.png);`">
+    <section class="area-main-text">
+      <div class="wrap-text-left">
+        <p class="color-point"><em>ONERPG</em></p>
+      </div>
+      <div class="wrap-text-right">
+        <p><em>대개편</em></p>
+      </div>
+    </section>
+    <div class="inner-size-basic">
+      <div class="wrap-quick-menu">
+        <!-- <section class="column-full point-banner">
+          <div class="box-flex">
+            <div class="item-flex area-img">
+              <img src="@/assets/images/character-update-220722.jpg" alt="">
             </div>
-            <div class="area-contents">
-              <character-search-box
-                :matchingData="userNickNames"
-              />
+            <div class="item-flex about">
+              <div class="area-text">
+                <h3 class="title">🤹‍♂️ 캐릭터 페이지에 기능이 추가되었습니다 🎉</h3>
+                <p>류오 / 시너지 / 스탯이 추가되었어요!</p>
+              </div>
+              <div class="area-contents">
+                <character-search-box
+                  :matchingData="userNickNames"
+                />
+              </div>
             </div>
           </div>
-        </div>
-      </section> -->
-      <!-- <section class="quick-menu column">
-        <div class="area-text">
-          <h3 class="title">캐릭터 검색</h3>
-          <p>다른 유저의 빌드를 참고해 보세요. 😎</p>
-        </div>
-        <div class="area-contents">
-          <character-search-box
-            :full-data="gameUsers"
-          />
-        </div>
-      </section> -->
-      <section class="quick-menu column">
-        <div class="area-text">
-          <h3 class="title">조합법 검색 👀</h3>
-          <p>조합법을 한눈에 볼 수 있습니다.</p>
-        </div>
-        <div class="area-contents">
-          <composition-search-box
-            :full-data="compositionItems"
-          />
-        </div>
-      </section>
-      <section class="quick-menu column">
-        <div class="area-text">
-          <h3 class="title">아이템 도감 📙</h3>
-          <p>등급 및 옵션을 필터링하고 <br>원하는 아이템 정보를 확인해보세요. </p>
-          <p class="wrap-links">
-            <router-link
-              v-for="({ pageName, text }) in itemPageLinks"
-              :key="`itemPageLink${pageName}`"
-              :to="`/items/${pageName}`"
-              class="text-link wrap-icon-text"
-            >
-              {{ text }}
-              <i class="icon-arrow right small border-point"></i>
-            </router-link>
-          </p>
-        </div>
-      </section>
-      <section class="quick-menu column bg-point item-bookmarks">
-        <template v-if="isLogin">
+        </section> -->
+        <!-- <section class="quick-menu column">
           <div class="area-text">
-            <h3 class="title small">
-              <span class="color-point"><strong>{{ userInfo && userInfo.siteNick }}</strong></span>님의 ⭐ 조합법
-            </h3>
+            <h3 class="title">캐릭터 검색</h3>
+            <p>다른 유저의 빌드를 참고해 보세요. 😎</p>
           </div>
           <div class="area-contents">
-            <item-bookmarks 
-              :show-tooltip="false"
+            <character-search-box
+              :full-data="gameUsers"
             />
           </div>
-        </template>
-        <div v-else class="area-text">
-          <h3 class="title">⭐ 조합법 즐겨찾기 기능 추가 ⭐</h3>
-          <p>
-            Google 계정으로 로그인하고 <br>
-            조합법 즐겨찾기 기능을 이용해 보세요! 
-          </p>
-        </div>
-      </section>
-      <!-- <section class="quick-menu column-full">
-        <div class="area-text">
-          <h3 class="title">랭킹 TOP 5</h3>
-          <p>오늘의 해적왕..🥶 원피스는 어디에 있나요..? 💎 </p>
-          <p>
-            <router-link
-              to="/ranking"
-              class="text-link wrap-icon-text"
-            >
-              전체 랭킹 보러가기
-              <i class="icon-arrow right small with-text border-point"></i>
-            </router-link>
-          </p>
-        </div>
-        <div class="area-contents">
-          <ranking-table 
-            :defaultDataNum="5"
-            :useInfiniteScroll="false"
-          />
-        </div>
-      </section> -->
+        </section> -->
+        <section class="quick-menu column">
+          <div class="area-text">
+            <h3 class="title">조합법 검색 👀</h3>
+            <p>조합법을 한눈에 볼 수 있습니다.</p>
+          </div>
+          <div class="area-contents">
+            <composition-search-box
+              :full-data="compositionItems"
+            />
+          </div>
+        </section>
+        <section class="quick-menu column">
+          <div class="area-text">
+            <h3 class="title">아이템 도감 📙</h3>
+            <p>등급 및 옵션을 필터링하고 <br>원하는 아이템 정보를 확인해보세요. </p>
+            <p class="wrap-links">
+              <router-link
+                v-for="({ pageName, text }) in itemPageLinks"
+                :key="`itemPageLink${pageName}`"
+                :to="`/items/${pageName}`"
+                class="text-link wrap-icon-text"
+              >
+                {{ text }}
+                <i class="icon-arrow right small border-point"></i>
+              </router-link>
+            </p>
+          </div>
+        </section>
+        <section class="quick-menu column item-bookmarks">
+          <template v-if="isLogin">
+            <div class="area-text">
+              <h3 class="title small">
+                <span class="color-point"><strong>{{ userInfo && userInfo.siteNick }}</strong></span>님의 ⭐ 조합법
+              </h3>
+            </div>
+            <div class="area-contents">
+              <item-bookmarks
+                :show-tooltip="false"
+              />
+            </div>
+          </template>
+          <div v-else class="area-text">
+            <h3 class="title">조합법 즐겨찾기 ⭐</h3>
+            <p>
+              Google 계정으로 로그인하고 <br>
+              조합법 페이지에서 즐겨찾기 기능을 이용해 보세요!
+            </p>
+          </div>
+        </section>
+        <!-- <section class="quick-menu column-full">
+          <div class="area-text">
+            <h3 class="title">랭킹 TOP 5</h3>
+            <p>오늘의 해적왕..🥶 원피스는 어디에 있나요..? 💎 </p>
+            <p>
+              <router-link
+                to="/ranking"
+                class="text-link wrap-icon-text"
+              >
+                전체 랭킹 보러가기
+                <i class="icon-arrow right small with-text border-point"></i>
+              </router-link>
+            </p>
+          </div>
+          <div class="area-contents">
+            <ranking-table
+              :defaultDataNum="5"
+              :useInfiniteScroll="false"
+            />
+          </div>
+        </section> -->
+      </div>
     </div>
   </div>
 </template>
@@ -149,7 +159,8 @@ export default {
           pageName: 'etcItem',
           text: '기타'
         },
-      ]
+      ],
+      BASE_URL: process.env.BASE_URL,
     }
   },
   computed: {
