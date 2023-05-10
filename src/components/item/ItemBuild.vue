1a2cb2ef806cac14d0ce5f6a5ad716d218a5bf86
정수진 <sssjsjj@ipartners.co.kr>
2023-05-10 17:35:24 +0900

    feat(style): 아이템 빌드 배치 조정, 전체 옵션값 관련 로직은 추후 작업 예정으로 히든처리

--- src/components/item/ItemBuild.vue
+++ src/components/item/ItemBuild.vue
@@ -1,65 +1,67 @@
 <template>
 <div>
   <div class="text-refer top">
+    <p v-if="buildData.saveDate">최근 세이브: {{ buildData.saveDate }}</p>
     <p class="align-right">
-      <span v-if="buildData.saveDate">최근 세이브: {{ buildData.saveDate }}</span>
       <span class="badge-text-wrap">
         <span class="badge black line-gold" data-v-21f97cac="">숫자</span>
         : 초월 수치
       </span>
     </p>
   </div>
-  <div v-if="itemGridInfoList" class="wrap-items-info">
-    <title-content
-      v-for="(itemGridInfo, i) in itemGridInfoList"
-      :key="`itemGridInfo${i}`"
-      :title="itemGridInfo.title"
-      :type="itemGridInfo.type"
+  <div class="wrapper">
+    <div
+      v-if="itemGridInfoList"
+      class="wrap-item-build"
     >
-      <item-list
-        :items="buildData[itemGridInfo.type]"
-        :type="itemGridInfo.type"
-        :columnNum="itemGridInfo.columnNum"
+      <div
+        v-for="({ title, type, columnNum }, i) in itemGridInfoList"
+        :key="`itemGridInfo${i}`"
+        class="wrap-title-content"
       >
-        <template v-slot="{ item }">
-          <item-box
-            :item="item"
-            :showBadges="['howGet', 'stack']"
-          ></item-box>
-        </template>
-      </item-list>
-    </title-content>
-    <div v-if="buildData.synergy.length > 0" class="area-synergies">
-      <synergy-desc
-        :synergies="buildData.synergy"
-      />
-    </div>
-    <template v-if="buildData.totalOption.length > 0">
-      <section class="all-options-main">
-        <h2 class="ir-hidden">빌드 총 스탯</h2>
-        <item-detail-info
-          type="total"
-          columns="3"
-          colorMode="white"
-          :options="buildData.totalOption.slice(0,12)"
-          :plusMinusUnit="false"
-          :showValueDecimal="!showRangeValue"
-          :showRangeValue="showRangeValue"
-        />
-        <p class="text-notice">실제 스탯과 약간의 오차가 있을 수 있습니다.</p>
-      </section>
-      <div class="all-options-sub">
-        <item-detail-info
-          type="total"
-          columns="1"
-          colorMode="white"
-          :options="buildData.totalOption.slice(12)"
-          :plusMinusUnit="false"
-          :showValueDecimal="!showRangeValue"
-          :showRangeValue="showRangeValue"
-        />
+        <title-content
+          :title="title"
+          :type="type"
+        >
+          <item-list
+            :items="buildData[type]"
+            :type="type"
+            :columnNum="columnNum"
+          >
+            <template v-slot="{ item }">
+              <item-box
+                :item="item"
+                :showBadges="['howGet', 'stack']"
+              ></item-box>
+            </template>
+          </item-list>
+        </title-content>
+        <div
+          v-if="type === 'sailor' && buildData.synergy.length > 0"
+          class="area-synergies"
+        >
+          <synergy-desc
+            :synergies="buildData.synergy"
+          />
+        </div>
       </div>
-    </template>
+    </div>
+    <!-- <section
+      v-if="buildData.totalOption.length > 0"
+      class="all-options-main"
+    >
+      <h2 class="ir-hidden">빌드 총 스탯</h2>
+      <item-detail-info
+        type="total"
+        columns="1"
+        colorMode="white"
+        :options="buildData.totalOption"
+        :plusMinusUnit="false"
+        :showValueDecimal="!showRangeValue"
+        :showRangeValue="showRangeValue"
+      />
+      <p class="text-notice">실제 스탯과 <br>약간의 오차가 있을 수 있습니다.</p>
+    </section> -->
   </div>
 </div>
 </template>
