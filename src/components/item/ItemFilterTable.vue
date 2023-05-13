2c73c24d1c89556b0e797e280700ad26dfd65fcd
정수진 <sssjsjj@ipartners.co.kr>
2023-05-13 19:48:03 +0900

    refactor: 도감 item filter table 컴포넌트 추출

---
+++ src/components/item/ItemFilterTable.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <section v-if="gradeMenus || optionMenus">
+      <h2 class="ir-hidden">필터 선택</h2>
+      <option-bar
+        v-if="gradeMenus"
+        title="등급"
+        :options="gradeMenus"
+        :select-list="gradesSelected"
+        @onChange="(list) => gradesSelected = list"
+      />
+      <option-bar
+        v-if="optionMenus"
+        title="옵션"
+        :options="optionMenus"
+        :select-list="optionsSelected"
+        @onChange="(list) => optionsSelected = list"
+      />
+    </section>
+    <div class="mrg-top-medium">
+      <item-table
+        type="equipment"
+        :items="tableItems"
+        :optionsSelected="optionsSelected"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import OptionBar from '@/components/common/OptionBar.vue';
+export default {
+  components: {
+    OptionBar
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+    optionMenus: {
+      type: Object,
+      default: () => null
+    },
+    gradeMenus: {
+      type: Object,
+      default: () => null
+    },
+  },
+  data() {
+    return {
+      gradesSelected: [],
+      optionsSelected: [],
+      tableItems: null
+    }
+  },
+  watch: {
+    optionsSelected(crr, pre) {
+      this.setTableItems()
+    },
+    gradesSelected(crr, pre) {
+      this.setTableItems()
+    }
+  },
+  created() {
+    this.tableItems = this.items
+  },
+  methods: {
+    setTableItems() {
+      const tableItems = this.items.filter(item => {
+        const { grade, option } = item
+        const isAllGrade = this.gradesSelected.length === 0
+        const isAllOption = this.optionsSelected.length === 0
+        const filteringGrade = isAllGrade ? true
+          : this.gradesSelected.includes(grade)
+        if(!filteringGrade) return false
+        const optionKeys = [...option].map(option => Object.keys(option)[0])
+        const checkListOptions = this.optionsSelected.map(optionsSelected => optionKeys.includes(optionsSelected))
+
+        const checkOptions = [...new Set(checkListOptions)]
+
+        const filteringOptions = isAllOption ? true
+          : checkOptions.length === 1 && checkOptions[0]
+        if(filteringOptions === true) return true
+      })
+      
+      this.tableItems = tableItems
+    },
+  }
+}
+</script>
+
+<style>
+</style>
--- src/pages/items/equipment.vue
+++ src/pages/items/equipment.vue
@@ -1,35 +1,16 @@
 <template>
   <section>
     <h2 class="ir-hidden">장비 아이템 도감</h2>
-    <section>
-      <h2 class="ir-hidden">필터 선택</h2>
-      <option-bar
-        v-if="gradeMenus"
-        title="등급"
-        :options="gradeMenus"
-        :select-list="gradesSelected"
-        @onChange="(list) => gradesSelected = list"
-      />
-      <option-bar
-        v-if="optionMenus"
-        title="옵션"
-        :options="optionMenus"
-        :select-list="optionsSelected"
-        @onChange="(list) => optionsSelected = list"
-      />
-    </section>
-    <div class="mrg-top-medium">  
-      <item-table
-        type="equipment"
-        :items="resultEquipments"
-        :optionsSelected="optionsSelected"
-      />
-    </div>
+    <item-filter-table
+      :items="equipments"
+      :option-menus="optionMenus"
+      :grade-menus="gradeMenus"
+    />
   </section>
 </template>
 
 <script>
-import OptionBar from '@/components/common/OptionBar.vue';
+import ItemFilterTable from '@/components/item/ItemFilterTable.vue';
 import setMeta from '@/plugins/utils/meta';
 import { noEquipOptions, gradesDef, equipmentGrades } from '@/plugins/utils/item-def'
 
@@ -42,7 +23,7 @@ export default {
     })
   },
   components: {
-    OptionBar
+    ItemFilterTable
   },
   async asyncData({ store }) {
     const equipments = await store.dispatch('item/GET_EQUIPMENTS_TABLE')
@@ -59,82 +40,6 @@ export default {
       gradeMenus
     }
   },
-  data() {
-    return {
-      gradesSelected: [],
-      optionsSelected: [],
-      resultEquipments: null
-    }
-  },
-  watch: {
-    optionsSelected(crr, pre) {
-      this.setResultEquipments()
-    },
-    gradesSelected(crr, pre) {
-      this.setResultEquipments()
-    }
-  },
-  created() {
-    console.log('this.equipments', this.equipments)
-    this.resultEquipments = this.equipments
-  },
-  methods: {
-    setResultEquipments() {
-      const resultEquipments = this.equipments.filter(equipment => {
-        const { grade, option } = equipment
-        const isAllGrade = this.gradesSelected.length === 0
-        const isAllOption = this.optionsSelected.length === 0
-        const filteringGrade = isAllGrade ? true
-          : this.gradesSelected.includes(grade)
-        if(!filteringGrade) return false
-        const optionKeys = [...option].map(option => Object.keys(option)[0])
-        const checkListOptions = this.optionsSelected.map(optionsSelected => optionKeys.includes(optionsSelected))
-
-// console.log('optionKeys', optionKeys, checkListOptions)
-        const checkOptions = [...new Set(checkListOptions)]
-
-        const filteringOptions = isAllOption ? true
-          : checkOptions.length === 1 && checkOptions[0]
-        if(filteringOptions === true) return true
-      })
-      
-      this.resultEquipments = resultEquipments
-    },
-    isActiveMenu(key, type) {
-      const selectList = this[`${type}sSelected`]
-      const isActiveMenu = key === 'all'
-        ? selectList.length === 0
-        : selectList.includes(key)
-      
-      return isActiveMenu
-    },
-    toggleMenu(key, type) {
-      const dataName = `${type}sSelected`
-      if(key === 'all') {
-        this[dataName] = []
-        return
-      }
-      if(this[dataName].includes(key)) {
-        const index = this[dataName].indexOf(key)
-        this[dataName].splice(index, 1)
-      } else {
-        this[dataName].push(key)
-      }     
-    },
-    classNegaPosi(equipment) {
-      return equipment.coloYn ? 'positive' : 'negative'
-    },
-    classColoPassive(coloPassive) {
-      switch (coloPassive) {
-        case '버프':
-          return 'buff'
-        case '자신':
-          return 'self'
-        case '디버프':
-          return 'deBuff'
-      }
-    }
-  }
 }
 </script>
 
