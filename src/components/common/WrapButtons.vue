3f18424d08f83be4b4a99e3c18854caeccf341f1
정수진 <sssjsjj@ipartners.co.kr>
2023-05-13 14:52:18 +0900

    feat: WrapButtons 컴포넌트 신규 생성

---
+++ src/components/common/WrapButtons.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    :class="`wrap-buttons size-${size} align-${align}`"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: String,
+      default: () => 'medium'
+    },
+    align: {
+      type: String,
+      default: () => 'center'
+    }
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrap-buttons {
+  display: flex;
+  &.size {
+    &-medium {
+      margin-top: 20px;
+      gap: 10px;
+    }
+  }
+  &.align {
+    &-center {
+      justify-content: center;
+    }
+  }
+}
+</style>
