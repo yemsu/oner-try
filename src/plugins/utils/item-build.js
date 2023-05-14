278c24eea96f4199bd81a025195b7e199e0864b8
정수진 <sssjsjj@ipartners.co.kr>
2023-05-14 17:06:45 +0900

    fix: item build 아이템들 max stack 기본 지정

--- src/plugins/utils/item-build.js
+++ src/plugins/utils/item-build.js
@@ -1,24 +1,26 @@
 import { getTotalOption, getCharacterSynergies } from '@/plugins/utils/character'
 import { fillDefaultList } from '@/plugins/utils/item'
 import { remakeDateStr } from '@/plugins/utils/index'
-import { slotNumbers } from '@/plugins/utils/item-def'
+import { slotNumbers, maxStack } from '@/plugins/utils/item-def'
 
 export const parseItemBuildData = (itemBuild, items, synergies, heroes) => {
   const getFullData = (itemId) => items.find(({id}) => id === itemId)
+  const parseItems = (type) => {
+    const itemBuildKeyName = ['equipment', 'sailor'].includes(type)
+      ? `${type}s` : type
+    return fillDefaultList(
+      itemBuild[itemBuildKeyName].split(',').map(getFullData),
+      slotNumbers[type]
+    ).map(item => {
+      item.stack = maxStack(item)
+      return item
+    })
+  }
   const hero = heroes.find(hero => hero.imageName === itemBuild.characterName)
   const regDt = remakeDateStr(itemBuild.regDt)
-  const equipment = fillDefaultList(
-    itemBuild.equipments.split(',').map(getFullData),
-    slotNumbers.equipment
-  )
-  const sailor = fillDefaultList(
-    itemBuild.sailor.split(',').map(getFullData),
-    slotNumbers.sailor
-  )
-  const ship = fillDefaultList(
-    [itemBuild.ship].map(getFullData),
-    slotNumbers.ship
-  )
+  const equipment = parseItems('equipment')
+  const sailor = parseItems('sailor')
+  const ship = parseItems('ship')
   const characterSynergies = getCharacterSynergies(sailor, synergies)
   const character = {...itemBuild, hero, regDt, equipment, sailor, ship, synergy: characterSynergies}
   const totalOption = getTotalOption(character, characterSynergies)
