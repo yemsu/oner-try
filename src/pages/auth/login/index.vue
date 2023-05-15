26bc0e47cd65a1c09c021c1a6ba77451899e2560
정수진 <sssjsjj@ipartners.co.kr>
2023-05-15 16:46:01 +0900

    refactor: ContentLayout 컴포넌트명 변경 및 auto import 사용

--- src/pages/auth/login/index.vue
+++ src/pages/auth/login/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="auth-login">
-    <content-layout
+    <layout-content-wrap
       size="basic"
       pd-top="none"
       :is-main-content="true"
@@ -10,19 +10,17 @@
       <google-login-button
         :is-in-page="true"
       />
-    </content-layout>
+    </layout-content-wrap>
   </section>
 </template>
 
 <script>
-import ContentLayout from '@/components/layout/ContentLayout.vue'
 import GoogleLoginButton from '@/components/layout/GoogleLoginButton.vue';
 import MainLogo from '@/components/common/MainLogo.vue';
 import { mapGetters } from 'vuex';
 
 export default {
   components: {
-    ContentLayout,
     GoogleLoginButton,
     MainLogo,
   },
