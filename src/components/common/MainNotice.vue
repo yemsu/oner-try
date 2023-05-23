70e56a8bcdcbc3ce529f512e689b5a9f086e8789
정수진 <sssjsjj@gmail.com>
2023-05-23 10:03:03 +0900

    refactor: 에러 페이지 내 메인 안내문구 영역 컴포넌트로 추출

---
+++ src/components/common/MainNotice.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="main-notice">
+    <h2 class="title">
+      <span v-if="pointText" class="point-text">{{ pointText }}</span>
+      {{ title }}
+    </h2>
+    <p class="desc">
+      <slot></slot>
+    </p>
+    <p v-if="subText" class="text-sub">{{ subText }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    pointText: {
+      type: String,
+      default: ''
+    },
+    subText: {
+      type: String,
+      default: ''
+    },
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.main-notice {
+  *:nth-child(n+2) {
+    margin-top: 20px;
+  }
+  .title {
+    font-size: var(--font-size-title-B);
+  }
+  .desc {
+    font-size: var(--font-size-B);
+    color: var(--font-dark-gray);
+  }
+  .text-sub {
+    font-size: var(--font-size-S);
+    color: var(--font-dark-gray);
+  }
+  .point-text {
+    display: block;
+    margin-bottom: 20px;
+    font-size: 90px;
+    color: var(--font-gray);
+    font-weight: var(--font-weight-B);
+    line-height: 1.2;
+    letter-spacing: 0.05em;
+  }
+}
+</style>
